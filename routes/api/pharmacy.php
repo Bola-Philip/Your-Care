a6b46dc31f04c780f0b4bfe4ba0fa0a148d335bb
SofyanAlhajji <alkadar117@gmail.com>
2023-06-25 21:04:30 +0300

    edit pharmacy

--- routes/api/pharmacy.php
+++ routes/api/pharmacy.php
@@ -18,13 +18,13 @@ Route::group(['middleware' => ['api','auth:pharmacy'], 'prefix' => 'pharmacy'], 
 
     Route::post('login', 'App\Http\Controllers\Api\PharmacyController@login')->withoutMiddleware('auth:pharmacy');
     Route::post('register', 'App\Http\Controllers\Api\PharmacyController@register')->withoutMiddleware('auth:pharmacy');
+    Route::post('show/{pharmacy_id}', 'App\Http\Controllers\Api\PharmacyController@show');
     Route::post('logout', 'App\Http\Controllers\Api\PharmacyController@logout');
     Route::post('refresh', 'App\Http\Controllers\Api\PharmacyController@refresh');
     Route::post('myData', 'App\Http\Controllers\Api\PharmacyController@myData');
     Route::post('edit', 'App\Http\Controllers\Api\PharmacyController@update');
     Route::post('addProduct', 'App\Http\Controllers\Api\PharmacyController@addProduct');
-    Route::post('show/{pharmacy_id}', 'App\Http\Controllers\Api\PharmacyController@show');
-    Route::post('addProductImage', 'App\Http\Controllers\Api\PharmacyController@addProductImage');
+    Route::post('addProductImage/{product_id}', 'App\Http\Controllers\Api\PharmacyController@addProductImage');
     Route::post('deleteMyAccount', 'App\Http\Controllers\Api\PharmacyController@destroy');
     Route::post('delete/{pharmacy_id}', 'App\Http\Controllers\Api\PharmacyController@destroy');
 
