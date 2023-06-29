7170c3b64fb3d54e3460a1a79b8a30071cd99d6d
Bola-Philip <bolaphilip3@gmail.com>
2023-06-29 19:12:11 +0300

    doctor make ads and some updates

---
+++ database/migrations/2023_06_27_195623_create_ads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('doctor_id');
+            $table->string('doctor_name');
+            $table->string('specialty');
+            $table->text('details');
+            $table->string('image')->nullable();
+            $table->timestamps();
+
+            $table->foreign('doctor_id')->references('id')->on('doctors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ads');
+    }
+};
